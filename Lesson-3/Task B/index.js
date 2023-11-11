2f0ba3c4d4cc8a8889cda1a29061dadea54eccfc
Roman Rozinko <r.rozinko@gmail.com>
2023-11-11 18:11:40 +0300

    add Lesson 3 task A, B

---
+++ Lesson-3/Task B/index.js
@@ -0,0 +1,45 @@
+const fs = require('fs')
+let fileContent = fs.readFileSync("input.txt", "utf8");
+const lines = fileContent.toString().split('\n')
+
+let [n, s, f] = lines[0].trim().split(' ').map(i => Number(i))
+let data = {}
+for (let i = 1; i <= n; i++) {
+	let nums = lines[i].trim().split(' ').map(i => Number(i))
+	data[i] = {
+		visited: false,
+		mincost: i == s ? 0 : -1,
+		prev: -1,
+		nexts: [-1, ...nums]
+	}
+}
+
+let next = s
+while (next !== -1 && !data[f].visited) {
+	data[next].visited = true
+	data[next].nexts.forEach((val, index) => {
+		if (val >= 0 && (data[next].mincost + val < data[index].mincost || data[index].mincost === -1)) {
+			data[index].mincost = data[next].mincost + val
+			data[index].prev = Number(next)
+		}
+	})
+	// find next
+	next = -1
+	nextcost = -1
+	Object.keys(data).forEach(key => {
+		if (!data[key].visited && data[key].mincost >= 0 && (data[key].mincost < nextcost || nextcost === -1)) {
+			next = key
+			nextcost = data[key].mincost
+		}
+	})
+}
+
+let steps = []
+let prev = f
+while (prev !== -1) {
+	steps.push(prev)
+	prev = data[prev].prev
+}
+
+let output = data[f].mincost >= 0 ? steps.reverse().map(a => a.toString()).join(' ') : '-1'
+fs.writeFileSync("output.txt", output)
